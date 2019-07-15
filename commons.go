83fd5bbf333e4d35b3bd801aadd7dfc0c409c674
webee.yw <webee.yw@gmail.com>
2019-07-15 11:23:57 +0800

    init

---
+++ commons.go
@@ -0,0 +1,38 @@
+package examples
+
+import (
+	"net/http"
+	// pprof
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// SetupSignal register signals handler and waiting for.
+func SetupSignal() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	for {
+		s := <-c
+		log.WithField("signal", s.String()).Info("signal")
+		switch s {
+		case os.Interrupt, syscall.SIGTERM:
+			return
+		default:
+			return
+		}
+	}
+}
+
+// StartPProfListen start debug pprof listening.
+func StartPProfListen(addr string) {
+	go func() {
+		log.Println("pprof listening:", addr)
+		if err := http.ListenAndServe(addr, nil); err != nil {
+			log.Panicln("pprof listening:", err)
+		}
+	}()
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/multisocket/examples
+
+go 1.12
+
+require (
+	github.com/multisocket/multisocket v0.0.0-20190715025634-65305f0b07cb
+	github.com/sirupsen/logrus v1.4.2
+)
